ad362d6c3b9c224bf4f79de9947f7014d5338cd5
Dawid <dawslusarek@gmail.com>
2021-04-07 11:40:43 +0200

    test

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ window.Pusher = require("pusher-js");
 
 window.Echo = new Echo({
     broadcaster: "pusher",
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: "9aa68f359304c1503467",
+    cluster: "eu",
     forceTLS: true,
 });
