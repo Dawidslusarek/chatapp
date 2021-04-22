e9bf5949743c50d01a9ad3e32fe457a5445e9d15
Dawid <dawslusarek@gmail.com>
2021-04-22 12:21:10 +0200

    Fix

--- database/migrations/2021_03_29_100451_create_messages_table.php
+++ database/migrations/2021_03_29_100451_create_messages_table.php
@@ -18,7 +18,7 @@ class CreateMessagesTable extends Migration
             $table->integer('room_id');
             $table->integer('user_id');
             $table->text('message');
-            $table->timestamp('published_at')->nullable();
+            $table->timestamp('published_at')->nullable()->default(1);
             $table->timestamps();
         });
     }
