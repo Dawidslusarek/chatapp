be543124d117c554ca5e6bb0acb3729647a81c67
Dawid <dawslusarek@gmail.com>
2021-05-02 13:27:53 +0200

    message length

--- resources/js/Pages/Chat/inputMessage.vue
+++ resources/js/Pages/Chat/inputMessage.vue
@@ -1,16 +1,17 @@
 <template charset="UTF-8">
-    <div style="border-top: 1px solid black" class="grid grid-cols-6">
+    <div style="border-top: 1px solid black" class="grid grid-cols-12">
+        <div class="maxlength" ><div class="input-group-addon" v-text="(max - message.length)"></div>/300</div>
         <input
             type="text"
-            class="col-span-5 p-2 outline-none border-none"
+            class="col-span-9 p-2 outline-none border-none"
             v-model="message"
             @keyup.enter="sendMessage()"
             placeholder="Napisz coś"
-            maxlength="250"
+            :maxlength="max"
         />
         <button
             @click="sendMessage()"
-            class="col-span-1 bg-gray-500 hover:bg-blue-700 p-2 rounded text-white"
+            class="col-span-2 bg-gray-500 hover:bg-blue-700 p-2 rounded text-white"
         >
             Wyślij
         </button>
@@ -22,15 +23,18 @@ import Input from "../../Jetstream/Input.vue";
 
 export default {
     components: { Input },
-    props: ["room", "message"],
+    props: ["room","message"],
     data() {
         return {
             message: "",
+            max: 300,
         };
     },
     methods: {
-        sendMessage() {
-            this.checkMessage();
+            sendMessage() {
+
+                this.checkLength();
+
 
             if (this.message == "") {
                 return;
@@ -49,51 +53,57 @@ export default {
                 .catch((error) => {
                     console.log(error);
                 });
+
+
         },
 
-        checkMessage() {
-            this.checkLength();
-            let blacklisted = require("./wulgaryzmy.json");
-            //2 looking for words
-            let foundInText = false;
-            let znaleziono = [];
-            let gwiazdki = "";
-            for (var i in blacklisted) {
-                // loops through the blacklisted list
-                if (
-                    this.message
-                        .toLowerCase()
-                        .includes(blacklisted[i].toLowerCase())
-                ) {
-                    znaleziono.push(blacklisted[i]);
-                    foundInText = true;
+                checkMessage(){
+                    this.checkLength();
+
+                        let blacklisted = require('./wulgaryzmy.json');
+                            //2 looking for words
+                            let foundInText = false;
+                            let znaleziono = [];
+                            let gwiazdki = "";
+                            for (var i in blacklisted) { // loops through the blacklisted list
+                            if (this.message.toLowerCase().includes(blacklisted[i].toLowerCase())){
+                                znaleziono.push(blacklisted[i]);
+                                foundInText = true;
+                                }
+                            }
+                            // checks casesensitive words
+
+                            //3 deletes and send message
+                            if (foundInText) {
+                                for(var i=0; i < znaleziono.length; i++) {
+                                    let gw=znaleziono[i];
+
+                                    for(var j=0; j< gw.length; j++)
+                                    {
+                                        gwiazdki += "*";
+                                    }
+                                     this.message = this.message.toLowerCase().replaceAll(gw, gwiazdki);
+                                     gwiazdki="";
+                                }
+                            }
+                },
+
+                checkLength() {
+                let mes = this.message;
+                if(mes.length>300) {
+                    this.message = mes.substring(0, 300);
                 }
-            }
-            // checks casesensitive words
+                },
 
-            //3 deletes and send message
-            if (foundInText) {
-                for (var i = 0; i < znaleziono.length; i++) {
-                    let gw = znaleziono[i];
-
-                    for (var j = 0; j < gw.length; j++) {
-                        gwiazdki += "*";
-                    }
-                    this.message = this.message
-                        .toLowerCase()
-                        .replaceAll(gw, gwiazdki);
-                    gwiazdki = "";
-                }
-            }
-        },
-        checkLength() {
-            let mes = this.message;
-            if (mes.length > 300) {
-                this.message = mes.substring(0, 300);
-            }
-        },
     },
 };
 </script>
 
-<style></style>
+<style>
+.maxlength{
+display:flex;
+align-items:center;
+justify-content:center;
+background-color:#ecf0f1;
+}
+</style>
