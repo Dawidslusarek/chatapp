2bc3bece49c6df9cbfe33eb02f0d6a0db919a599
Dawid <dawslusarek@gmail.com>
2021-06-28 19:23:23 +0200

    'branch'

--- resources/js/Pages/Chat/inputMessage.vue
+++ resources/js/Pages/Chat/inputMessage.vue
@@ -103,7 +103,7 @@ export default {
                     this.message = this.message
                         .toLowerCase()
                         .replaceAll(gw, gwiazdki);
-                    gwiazdki = "";
+                    gwiazdki = ""; 
                 }
             }
         },
